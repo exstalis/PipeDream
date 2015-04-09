b4ea79beb64e0cf4b3ee4777c447d9a47041e724
elif ece arslan <elifecearslan@exstalis.local>
2015-04-09 15:51:23 -0400

    still working on drawer..

---
+++ PipeDream/PDDrawerCustomCellBackground.m
@@ -0,0 +1,21 @@
+//
+//  PDDrawerCustomCellBackground.m
+//  PipeDream
+//
+//  Created by elif ece arslan on 4/9/15.
+//  Copyright (c) 2015 elif ece arslan. All rights reserved.
+//
+
+#import "PDDrawerCustomCellBackground.h"
+
+@implementation PDDrawerCustomCellBackground
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
