31a96006b1586f9225f9372640c13ec185e548ef
elif ece arslan <elifecearslan@exstalis.local>
2015-04-10 00:25:25 -0400

    basic implementation for base controllers and animations completed

---
+++ PipeDream/PDBaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDBaseTableViewController.h
+//  PipeDream
+//
+//  Created by elif ece arslan on 4/9/15.
+//  Copyright (c) 2015 elif ece arslan. All rights reserved.
+//
+
+#import "PDBaseViewController.h"
+
+@interface PDBaseTableViewController : PDBaseViewController
+
+@end
