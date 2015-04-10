39dc092e71124aef93af5f113c72a16dbad5b3ee
elif ece arslan <elifecearslan@exstalis.local>
2015-04-09 21:14:17 -0400

    autolayout for 4.7,4.0,ipad and landscape mode fixed

---
+++ PipeDream/PDDrawerTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PDDrawerTableViewCell.h
+//  PipeDream
+//
+//  Created by elif ece arslan on 4/9/15.
+//  Copyright (c) 2015 elif ece arslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDDrawerTableViewCell : UITableViewCell
+
+@end
---
+++ PipeDream/PDDrawerTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PDDrawerTableViewCell.m
+//  PipeDream
+//
+//  Created by elif ece arslan on 4/9/15.
+//  Copyright (c) 2015 elif ece arslan. All rights reserved.
+//
+
+#import "PDDrawerTableViewCell.h"
+
+@implementation PDDrawerTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
