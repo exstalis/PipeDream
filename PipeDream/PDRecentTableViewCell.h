da1419c9bcafd74737ad65ea900515f92936c1bf
elif ece arslan <elifecearslan@exstalis.local>
2015-04-26 23:07:16 -0400

    started to implement detail views- custom scroll view

---
+++ PipeDream/PDRecentTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PDRecentTableViewCell.h
+//  PipeDream
+//
+//  Created by elif ece arslan on 4/26/15.
+//  Copyright (c) 2015 elif ece arslan. All rights reserved.
+//
+
+#import "PDFeedTableViewCell.h"
+
+@interface PDRecentTableViewCell : PDFeedTableViewCell
+
+@end
---
+++ PipeDream/PDRecentTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  PDRecentTableViewCell.m
+//  PipeDream
+//
+//  Created by elif ece arslan on 4/26/15.
+//  Copyright (c) 2015 elif ece arslan. All rights reserved.
+//
+
+#import "PDRecentTableViewCell.h"
+
+@implementation PDRecentTableViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
