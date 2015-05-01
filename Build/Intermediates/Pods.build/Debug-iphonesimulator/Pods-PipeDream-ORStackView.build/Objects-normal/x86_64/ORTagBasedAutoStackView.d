e1f1a2cb5488e329dccd7d942d3db346a9073cd0
elif ece arslan <elifecearslan@exstalis.local>
2015-04-30 22:49:20 -0400

    commit before merge

--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-PipeDream-ORStackView.build/Objects-normal/x86_64/ORTagBasedAutoStackView.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/elifecearslan/Developer/PipeDream/Pods/ORStackView/Classes/ios/ORTagBasedAutoStackView.m \
-  /Users/elifecearslan/Developer/PipeDream/Pods/Target\ Support\ Files/Pods-PipeDream-ORStackView/Pods-PipeDream-ORStackView-prefix.pch \
-  /Users/elifecearslan/Developer/PipeDream/Pods/Target\ Support\ Files/Pods-PipeDream/Pods-PipeDream-environment.h \
-  /Users/elifecearslan/Developer/PipeDream/Pods/ORStackView/Classes/ios/ORTagBasedAutoStackView.h \
-  /Users/elifecearslan/Developer/PipeDream/Pods/ORStackView/Classes/ios/ORStackView.h \
-  /Users/elifecearslan/Developer/PipeDream/Pods/ORStackView/Classes/ios/private/ORStackView+Private.h
